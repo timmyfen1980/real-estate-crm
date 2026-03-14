0016f9f6b57b12b9ece75dc2779221035ecffba1
Mary Fennessey <mary@example.com>
2026-03-14 06:06:36 -0400

    debug supabase key

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,6 +3,9 @@ import { createBrowserClient } from '@supabase/ssr'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ""
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
 
+console.log("SUPABASE URL:", supabaseUrl)
+console.log("SUPABASE KEY:", supabaseAnonKey)
+
 export const supabase = createBrowserClient(
   supabaseUrl,
   supabaseAnonKey
